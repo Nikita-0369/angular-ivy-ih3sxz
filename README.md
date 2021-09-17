cd0769de12863a7a7a50f5ea70792dcab05a621d
Nikita-0369 <nikita05gupta1996@gmail.com>
2021-09-17 15:07:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ih3sxz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ih3sxz)
---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css'],
+})
+export class HeroFormComponent {
+  constructor() {}
+
+  powers = ['Really smart', 'super hot', 'super flexible', 'whether change'];
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+}
